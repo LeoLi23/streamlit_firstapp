f1b2d9567cb34f80f90b45317acd74a7d974675b
Anyang Li <lianyang617@gmail.com>
2020-08-27 01:28:49 +0800

    f

--- manage_db.py
+++ manage_db.py
@@ -1,13 +1,16 @@
 # DB
 import sqlite3
 conn = sqlite3.connect('userdata.db',check_same_thread=False)
+conn2 = sqlite3.connect("result.db",check_same_thread=False)
 c = conn.cursor()
-
+c2 = conn2.cursor()
 # Functions
 
 def create_usertable():
     c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,email TEXT,password TEXT)')
 
+def create_usertable_prediction():
+    c2.execute('CREATE TABLE IF NOT EXISTS userstable(Cycle TEXT,Age TEXT,Years_home TEXT,Years_business TEXT,Cashflow TEXT,Collateral TEXT,Loan_amount TEXT,Default_rate TEXT,Suggestion TEXT)')
 
 def add_username(username,password):
     c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
@@ -17,6 +20,12 @@ def add_user_email(email,password):
     c.execute('INSERT INTO userstable(email,password) VALUES (?,?)',(email,password))
     conn.commit()
 
+def add_to_result(Cycle,Age,Years_home,Years_business,Cashflow,Collateral,Loan_amount,Default_rate,Suggestion):
+    c2.execute('INSERT INTO userstable(Cycle,Age,Years_home,Years_business,Cashflow,Collateral,Loan_amount,Default_rate,Suggestion) VALUES (?,?,?,?,?,?,?,?,?)',(
+        Cycle, Age, Years_home, Years_business, Cashflow, Collateral, Loan_amount, Default_rate,Suggestion
+    ))
+    conn2.commit()
+    
 def login_user(username = None,email = None,password = None):
     if username is not None and password is not None and email is None:#use username
         c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
@@ -47,8 +56,5 @@ def view_all_users():
     return data
 
 if __name__=='__main__':
-    create_usertable()
-    print(login_user(username = 'leo',password = 1234))
-    print(login_user(email = 'leo23@gmail.com',password =1234 ))
-    #add_userdata('Leo',,12345)
-    #check_unique('Leo','123@gmail.com')
+    create_usertable_prediction()
+    add_to_result(12,23,4,4,100000,1000000,200000,0.44,"Recommended")
